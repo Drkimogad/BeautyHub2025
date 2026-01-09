9f9eb3d56253c194ecef470d8f23e59ec355c8d1
Karim Gad ( Veterinarian) <dr_kimogad@yahoo.com>
2026-01-09 13:49:26 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 // firebase-config.js
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB96xSqgk4gI1Z94ZXSESFlfbVMb3meo7U",
+  authDomain: "beautyhub-5987d.firebaseapp.com",
+  projectId: "beautyhub-5987d",
+  storageBucket: "beautyhub-5987d.firebasestorage.app",
+  messagingSenderId: "20539362118",
+  appId: "1:20539362118:web:b15665bb331433c1ea7347",
+  measurementId: "G-G1YQSSM8TK"
 };
 
 // Initialize Firebase
@@ -13,4 +14,5 @@ firebase.initializeApp(firebaseConfig);
 
 // Export services
 const auth = firebase.auth();
+
 const firestore = firebase.firestore();
