f1db4290a48b1698a6fc824e029c037ef02f345b
Karim Gad ( Veterinarian) <dr_kimogad@yahoo.com>
2026-01-12 20:10:15 +0200

    Update customerorder.js

--- js/customerorder.js
+++ js/customerorder.js
@@ -1,4 +1,4 @@
-// customerorder.js - Customer Order Form & Submission
+// customerorder.js - Customer Order Form & Submission.
 const CustomerOrderManager = (function() {
     // DOM Elements
     let checkoutModal = null;
